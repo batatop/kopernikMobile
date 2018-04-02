f0cd6b759158dd041c3cf6cbcf2e87830bf69f10
Berkay Atatop <batatop@gmail.com>
2018-04-02 12:08:53 -0400

    added ugur's calendar

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -1,51 +1,181 @@
 import React from 'react';
-import { SafeAreaView, Text, StatusBar, Image, AppRegistry, ScrollView, StyleSheet, TouchableHighlight } from 'react-native';
+import { SafeAreaView, ActivityIndicator, View, ListView, Text, StatusBar, Image, AppRegistry, ScrollView, StyleSheet, TouchableHighlight, FlatList } from 'react-native';
 import glamorous from "glamorous-native";
 import { Calendar, Agenda } from 'react-native-calendars';
-import { bColor, pColor, pLightColor, pDarkColor, hColor } from "../style/colors"
+import { pColor, pLightColor, sColor } from "../style/colors"
+
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1; //January is 0!
+var year = today.getFullYear();
+
+if (day < 10) {
+    day = '0' + day
+}
+
+if (month < 10) {
+    month = '0' + month
+}
+
+today = year + '-' + month + '-' + day;
+postsl = "https://www.googleapis.com/calendar/v3/calendars/3phl0f0rmkj3st8ahuhhss19a0@group.calendar.google.com/events?key=AIzaSyBcugNSaHaIf7j5gYPFs524Aw8HnHsopJU&singleEvents=true&orderBy=startTime&timeMin=" + year + "-" + month + "-" + day + "T00:00:00Z&timeMax=" + year + 2 + "-" + "01-01T00:00:00Z"
 
 export default class AppCalendar extends React.Component {
-    static navigationOptions = ({ screenProps, navigation }) => ({
+    static navigationOptions = ({ navigation }) => ({
         title: "Calendar",
-        headerLeft: (
-            <TouchableHighlight
-                style={{ padding: 18 }}
-                onPress={() => screenProps.rootNavigation.navigate("DrawerToggle")}
-                underlayColor={hColor}
-            >
-                <Image source={require("../assets/menuIcon.png")} />
-            </TouchableHighlight>
-        ),
     });
 
-    componentWillMount() {
-        let calendar = "https://www.googleapis.com/calendar/v3/calendars/3phl0f0rmkj3st8ahuhhss19a0@group.calendar.google.com/events?key=AIzaSyBcugNSaHaIf7j5gYPFs524Aw8HnHsopJU"
-        fetch(calendar)
-        .then((res) => {
-            console.log(res)
-        })
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true,
+            events: [],
+            items: {}
+        }
+    }
+
+    componentDidMount() {
+        let postsUrl = "https://www.googleapis.com/calendar/v3/calendars/3phl0f0rmkj3st8ahuhhss19a0@group.calendar.google.com/events?key=AIzaSyBcugNSaHaIf7j5gYPFs524Aw8HnHsopJU&singleEvents=true&orderBy=startTime&timeMin=" + year + "-" + month + "-" + day + "T00:00:00Z&timeMax=" + year + 2 + "-" + "01-01T00:00:00Z"
+        fetch(postsUrl)
+            .then((response) => response.json())
+            .then((response) => {
+                var standartDataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+                this.setState({
+                    isLoading: false,
+                    events: standartDataSource.cloneWithRows(response)
+                })
+
+            })
+    }
+
+    handleEvents() {
+        var confirmedEvents = 0
+        var cancelledEvents = 0
+
+        let e = []
+        if (this.state.events != 0) {
+            for (let i = 0; i < this.state.events._dataBlob.s1.items.length; i++) {
+                if (this.state.events._dataBlob.s1.items[i].status == "confirmed") {
+                    let newEvent = {}
+                    newEvent.title = this.state.events._dataBlob.s1.items[i].summary
+                    newEvent.location = this.state.events._dataBlob.s1.items[i].location
+                    var start = this.state.events._dataBlob.s1.items[i].start.date || this.state.events._dataBlob.s1.items[i].start.dateTime
+                    var splitData = start.split('-')
+                    var splitforDay = splitData[2].split(':')
+                    var splitYear = splitData[0]
+                    var splitMonth = splitData[1]
+                    var splitDay = splitforDay[0][0] + splitforDay[0][1]
+                    start = splitYear + '-' + splitMonth + '-' + splitDay
+                    newEvent.start = start
+                    var end = this.state.events._dataBlob.s1.items[i].end.date || this.state.events._dataBlob.s1.items[i].end.dateTime
+                    splitData = end.split('-')
+                    splitforDay = splitData[2].split(':')
+                    splitYear = splitData[0]
+                    splitMonth = splitData[1]
+                    splitDay = splitforDay[0][0] + splitforDay[0][1]
+                    end = splitYear + '-' + splitMonth + '-' + splitDay
+                    newEvent.end = end
+                    e.push(newEvent)
+                    confirmedEvents++;
+                }
+                else {
+                    cancelledEvents++;
+                }
+            }
+            return e
+        }
+        else {
+            return []
+        }
     }
 
     render() {
         return (
-            <Container>
-                <StatusBar
-                    backgroundColor={pDarkColor}
-                    barStyle="light-content"
-                />
-                <Agenda
-                    style={{height: "100%"}}
-                    theme={{
-                        selectedDayBackgroundColor: pColor,
-                        todayTextColor: pLightColor,
-                        agendaKnobColor: pColor
-                    }}
-                />
-            </Container>
+            <Agenda
+                items={this.state.items}
+                loadItemsForMonth={this.loadItems.bind(this)}
+                selected={today}
+                renderItem={this.renderItem.bind(this)}
+                renderEmptyDate={this.renderEmptyDate.bind(this)}
+                rowHasChanged={this.rowHasChanged.bind(this)}
+                theme={{
+                    selectedDayBackgroundColor: pColor,
+                    todayTextColor: sColor,
+                    agendaKnobColor: pColor,
+                    agendaTodayColor: sColor,
+                    dotColor: pColor,
+                }}
+            />
+        )
+    }
+
+    loadItems(day) {
+        if (this.state.events.length != 0) {
+            data = this.handleEvents()
+            setTimeout(() => {
+                for (let i = -15; i < 85; i++) {
+                    const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                    const strTime = this.timeToString(time);
+                    if (!this.state.items[strTime]) {
+                        this.state.items[strTime] = [];
+                        const numItems = data.length;
+                        for (let j = 0; j < numItems; j++) {
+                            if (data[j].start == strTime) {
+                                this.state.items[strTime].push({
+                                    name: data[j].title + strTime,
+                                    height: Math.max(50, Math.floor(Math.random() * 150))
+                                });
+                            }
+                            else {
+                                // console.log('zaman', strTime)
+                            }
+                        }
+                    }
+                }
+                
+                const newItems = {};
+                Object.keys(this.state.items).forEach(key => { newItems[key] = this.state.items[key]; });
+                this.setState({
+                    items: newItems
+                });
+            }, 1000);
+        }
+    }
+
+    renderItem(item) {
+        return (
+            <View style={[styles.item, { height: item.height }]}><Text>{item.name}</Text></View>
         );
+    }
+
+    renderEmptyDate() {
+        return (
+            <View style={styles.emptyDate}><Text> </Text></View>
+        );
+    }
+
+    rowHasChanged(r1, r2) {
+        return r1.name !== r2.name;
+    }
+
+    timeToString(time) {
+        const date = new Date(time);
+        return date.toISOString().split('T')[0];
     }
 }
 
-const Container = glamorous.safeAreaView({
-    flex: 1,
-})
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17
+    },
+    emptyDate: {
+        height: 15,
+        flex: 1,
+        paddingTop: 30
+    }
+});
--- src/screens/ClearSky.js
+++ src/screens/ClearSky.js
@@ -2,74 +2,35 @@ import React from 'react';
 import { Image, WebView,Linking, SafeAreaView, ImageBackground, View, Text, StatusBar, AppRegistry, ScrollView, StyleSheet, TouchableHighlight, FlatList,Dimensions } from 'react-native';
 import glamorous from "glamorous-native";
 import { Calendar, Agenda } from 'react-native-calendars';
-import { bColor, pColor } from "../style/colors"
-
-//import Image from 'react-native-transformable-image'
-
-
-
-
-
+import { bColor, pColor, black } from "../style/colors"
 
 export default class AppCalendar extends React.Component {
     static navigationOptions = ({ navigation }) => ({
         title: "Clear Sky Chart",
     });
 
-    constructor(props){
-        super(props)
-        this.state= {
-             imgWidth: 0,
-             imgHeight: 0
-        }
-    }
-
-    componentDidMount(){
-    Image.getSize('http://www.cleardarksky.com/c/KprnkObNYcsk.gif?c=2714406', (width, height) => {
-      // calculate image width and height 
-      const screenWidth = Dimensions.get('window').width
-      const scaleFactor = width / screenWidth
-      const imageHeight = height / scaleFactor
-      this.setState({imgWidth: screenWidth, imgHeight: imageHeight})
-    })
-    }
-    
-
-
-
-
-
     render() {
            return (
-                <Image source={{uri: 'http://www.cleardarksky.com/c/KprnkObNYcsk.gif?c=2714406'}} style={s.backgroundImage} />
+                <Container>
+                    <ClearSkyImg source={{uri: 'http://www.cleardarksky.com/c/KprnkObNYcsk.gif?c=2714406'}} />
+                </Container>
             );
-        
     }
-
 }
 
-const win = Dimensions.get('window');
-const s = StyleSheet.create({
-  backgroundImage: {
-    flex:1,
+const Container = glamorous.safeAreaView({
+    flex: 1,
+    backgroundColor: black
+})
+
+const ClearSkyImg = glamorous.image({
+    flex: 1,
     alignSelf: 'center',
     height: 605,
     width: 550,
-    justifyContent:'center',
-     backgroundColor: '#000000',
-     //borderColor: '#F44336',
-    //borderWidth: 1,
-     borderRadius: 1,
-    transform:[{rotate:'90deg'}],
-    resizeMode:'contain',
-    
-  }
-});
-
-
-
-
-
-
-
-
+    justifyContent: 'center',
+    backgroundColor: '#000000',
+    borderRadius: 1,
+    transform: [{ rotate: '90deg' }],
+    resizeMode: 'contain',
+})
